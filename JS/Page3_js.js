6f1343ea3f6dcfefc7c9235b804eda8e7f95525e
Emile <emile.cap@icloud.com>
2025-10-03 19:15:26 +0200

    modification page3 + le css + js + ajoue d'image

--- JS/Page3_js.js
+++ JS/Page3_js.js
@@ -1,4 +1,24 @@
 function TogglePopUp1(){
-    let popup = document.querySelector("#popUpProduit");
+    let popup = document.querySelector("#popUpProduit1");
+    popup.classList.toggle("open")
+}
+
+function TogglePopUp2(){
+    let popup = document.querySelector("#popUpProduit2");
+    popup.classList.toggle("open")
+}
+
+function TogglePopUp3(){
+    let popup = document.querySelector("#popUpProduit3");
+    popup.classList.toggle("open")
+}
+
+function TogglePopUp4(){
+    let popup = document.querySelector("#popUpProduit4");
+    popup.classList.toggle("open")
+}
+
+function TogglePopUp5(){
+    let popup = document.querySelector("#popUpProduit5");
     popup.classList.toggle("open")
 }
